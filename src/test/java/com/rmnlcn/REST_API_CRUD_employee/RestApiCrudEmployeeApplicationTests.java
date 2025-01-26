ef2788269677ce61f7141c82de00a2beb4daeed6
RmNLcN06 <romain.luciano@orange.fr>
2025-01-26 22:57:41 +0100

    Initial commit

---
+++ src/test/java/com/rmnlcn/REST_API_CRUD_employee/RestApiCrudEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmnlcn.REST_API_CRUD_employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCrudEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
