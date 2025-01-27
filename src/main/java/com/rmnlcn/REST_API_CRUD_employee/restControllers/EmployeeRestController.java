f5723c59830ab93a8737b088e8d4cd00e844a388
RmNLcN06 <romain.luciano@orange.fr>
2025-01-27 15:54:48 +0100

    Add new methods (CRUD) to EmployeeDAO implementation & new Service methods

--- src/main/java/com/rmnlcn/REST_API_CRUD_employee/restControllers/EmployeeRestController.java
+++ src/main/java/com/rmnlcn/REST_API_CRUD_employee/restControllers/EmployeeRestController.java
@@ -3,9 +3,7 @@ package com.rmnlcn.REST_API_CRUD_employee.restControllers;
 import com.rmnlcn.REST_API_CRUD_employee.entities.Employee;
 import com.rmnlcn.REST_API_CRUD_employee.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,9 +19,55 @@ public class EmployeeRestController {
         employeeService = theEmployeeService;
     }
 
-    // expose "/employees" endpoint - return a list of employees
+    // expose GET "/employees" endpoint - return a list of employees
     @GetMapping("/employees")
-    public List<Employee> findAll() {
+    public List<Employee> findAllEmployees() {
         return employeeService.findAll();
     }
+
+    // expose GET "/employees/{employeeId}" endpoint - return employee by id
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployeeById(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if(theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return theEmployee;
+    }
+
+    // expose POST "/employees" endpoint - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+
+        theEmployee.setId(0);
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
+    // expose PUT "/employees" endpoint - update existing employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    // expose DELETE "/employees/{employeeId}" endpoint - delete existing employee
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+        if(tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
+    }
+
 }
