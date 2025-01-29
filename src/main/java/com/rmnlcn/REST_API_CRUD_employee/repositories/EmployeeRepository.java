d63c4127476c43374c50f7d6fb43d07df3322282
RmNLcN06 <romain.luciano@orange.fr>
2025-01-29 17:22:28 +0100

    Change Rest resource path

--- src/main/java/com/rmnlcn/REST_API_CRUD_employee/repositories/EmployeeRepository.java
+++ src/main/java/com/rmnlcn/REST_API_CRUD_employee/repositories/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.rmnlcn.REST_API_CRUD_employee.repositories;
 
 import com.rmnlcn.REST_API_CRUD_employee.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// @RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
