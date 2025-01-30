ca1bbe25ebd85ae3eba8acd2b894ff0955cf2b27
RmNLcN06 <romain.luciano@orange.fr>
2025-01-30 15:20:09 +0100

    Update Spring Security Configuration to use JDBC

--- src/main/java/com/rmnlcn/REST_API_CRUD_employee/securities/ProjectSecurityConfig.java
+++ src/main/java/com/rmnlcn/REST_API_CRUD_employee/securities/ProjectSecurityConfig.java
@@ -5,14 +5,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
+/*import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;*/
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
 
+    // add support for JDBC
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    /*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
 
@@ -41,7 +52,7 @@ public class ProjectSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(isabelle, aurelie, romain, florian);
-    }
+    }*/
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
