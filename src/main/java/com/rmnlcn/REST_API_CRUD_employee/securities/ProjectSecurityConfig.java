d55735a9db3c351885e0507669ed918b664e0441
RmNLcN06 <romain.luciano@orange.fr>
2025-01-30 11:34:40 +0100

    Restrict access based on roles

--- src/main/java/com/rmnlcn/REST_API_CRUD_employee/securities/ProjectSecurityConfig.java
+++ src/main/java/com/rmnlcn/REST_API_CRUD_employee/securities/ProjectSecurityConfig.java
@@ -2,9 +2,13 @@ package com.rmnlcn.REST_API_CRUD_employee.securities;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -37,5 +41,27 @@ public class ProjectSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(isabelle, aurelie, romain, florian);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/magic-api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/magic-api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/magic-api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/magic-api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/magic-api/employees/**").hasRole("ADMIN")
+        );
+
+
+        // use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable Cross Site Request Forgery (CSRF)
+        // in general, not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
