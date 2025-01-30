2fb75d8012b7f39a98cacc752b05afe603ed5458
RmNLcN06 <romain.luciano@orange.fr>
2025-01-30 10:10:03 +0100

    Add users, passwords and roles

--- src/main/java/com/rmnlcn/REST_API_CRUD_employee/securities/ProjectSecurityConfig.java
+++ src/main/java/com/rmnlcn/REST_API_CRUD_employee/securities/ProjectSecurityConfig.java
@@ -1,7 +1,41 @@
 package com.rmnlcn.REST_API_CRUD_employee.securities;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        UserDetails isabelle = User.builder()
+                .username("isabelle")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails aurelie = User.builder()
+                .username("aurelie")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails romain = User.builder()
+                .username("romain")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails florian = User.builder()
+                .username("florian")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(isabelle, aurelie, romain, florian);
+    }
 }
